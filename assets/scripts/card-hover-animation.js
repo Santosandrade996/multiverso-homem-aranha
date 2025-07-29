c8bf467b4fac0149f0e79493ebbb057e97ba9047
giselle-dio <giselleandrade1996@gmail.com>
2025-07-29 10:52:14 -0300

    Adicionando funcionalidades em javascript

---
+++ assets/scripts/card-hover-animation.js
@@ -0,0 +1,24 @@
+//Criando função para passar o mouse em para entrar em cada carta
+function handleMouseEnter() {
+    this.classList.add('s-card--hovered');
+    document.body.id = `$(this.id)hovered`;
+}
+
+//criando função para quando o mouse sai da carta
+function handleMouseLeave() {
+    this.classList.remove('s-card--hovered');
+    document.body.id = '';
+}
+
+//criando função para adicionar eventos do mouse
+function addEventListenersToCards() {
+  const cardElements = document.getElementsByClassName('s-card');
+  
+  for (let index = 0; index < cardElements.length; index++) {
+    const card = cardElements[index];
+    card.addEventListener('mouseenter', handleMouseEnter);
+    card.addEventListener('mouseleave', handleMouseLeave);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", addEventListenersToCards, false);
